c898748c9d68b707b11d03ae4bf5c8f080acc3d1
Nikita Romanov <nick_romanov123@hotmail.com>
2022-08-11 03:06:36 +0300

    fix gitignore

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,9 +5,11 @@ import { all } from 'redux-saga/effects';
 import { IPostsState } from './types';
 import { postReducer } from './reducer';
 import { PostRootSaga } from "./root-saga";
+import { modalReducer, ModalStoreType } from "../ui-library/src/redux/modal/reducer";
 
 export interface IRootState {
   PostStore: IPostsState;
+  ModalStore: ModalStoreType
 }
 
 const sagaMiddleware = createSagaMiddleware();
@@ -16,6 +18,7 @@ const middlewares = [sagaMiddleware];
 const store = createStore<IRootState, any, any, any>(
   combineReducers({
     PostStore: postReducer,
+    ModalStore: modalReducer
   }),
   undefined,
   applyMiddleware(...middlewares)
